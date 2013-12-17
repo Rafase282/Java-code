c896eb370ff6116f37005a8760c6b672e760742c
Rafael J. Rodriguez <rafase282@gmail.com>
2013-12-17 16:02:40 -0500

    Adding a new file

---
+++ Addition.java
@@ -0,0 +1,22 @@
+/**
+ * @author rafase282
+ * Rafael J. Rodriguez
+ * CMP 277
+ * rafase282@gmail.com
+ * Program that adds two numbers by user input.
+ */
+public class Addition {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number1 = Integer.parseInt(args [0]);
+		int number2 = Integer.parseInt(args [1]);
+		int result = number1 + number2;
+		System.out.println(result);
+
+	}
+
+}
